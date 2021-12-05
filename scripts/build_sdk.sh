0e714d0203004a4faaf4e2bf330fb809fc8f572e
Artur Rudiuk <a.rudiuk@criteo.com>
2021-12-05 16:52:21 +0200

    Update build_sdk.sh

--- scripts/build_sdk.sh
+++ scripts/build_sdk.sh
@@ -4,13 +4,25 @@ for dir in ../sdks/*;
 			then (
 				echo "$dir" &&  		
 				cd "$dir" && 			
-				python -m build --sdist --wheel --outdir ../../packages/
+				python -m build --sdist --wheel --outdir packages/ &&
+                python -m build --sdist --wheel --outdir ../packages/ &&
+                python -m build --sdist --wheel --outdir ../../packages/
 			);
 		fi
 	); 
 done
 
 for dir in ./*;
+	do ( 
+		if [ -d "$dir" ]; 			
+			then (
+				echo "$dir"
+			);
+		fi
+	); 
+done
+
+for dir in ./sdks/*;
 	do ( 
 		if [ -d "$dir" ]; 			
 			then (
