0650ed089801106858a4fb887f11c47156d99978
Artur Rudiuk <a.rudiuk@criteo.com>
2021-12-05 17:24:26 +0200

    test

--- scripts/build_sdk.sh
+++ scripts/build_sdk.sh
@@ -2,9 +2,9 @@ for dir in ../sdks/*;
 	do ( 
 		if [ -d "$dir" ]; 			
 			then (
-				echo "$dir" &&  		
-				cd "$dir" && 			
-				python -m build --sdist --wheel --outdir packages/
+				echo "$dir" 		
+				cd "$dir"			
+				python -m build --sdist --wheel --outdir ../../packages/
 			);
 		fi
 	); 
