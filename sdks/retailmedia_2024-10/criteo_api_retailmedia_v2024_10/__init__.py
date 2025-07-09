9e28c95bb30b8843a54d1f718008beb221e39546
gricher-crto <{actor}@users.noreply.github.com>
2025-07-09 17:05:33 +0000

    [250709] Automatic update of SDK.

--- sdks/retailmedia_2024-10/criteo_api_retailmedia_v2024_10/__init__.py
+++ sdks/retailmedia_2024-10/criteo_api_retailmedia_v2024_10/__init__.py
@@ -8,7 +8,7 @@
 """
 
 
-__version__ = "2024.10.0.250708"
+__version__ = "2024.10.0.250709"
 
 # import ApiClient
 from criteo_api_retailmedia_v2024_10.api_client import ApiClient
--- sdks/retailmedia_preview/criteo_api_retailmedia_preview/__init__.py
+++ sdks/retailmedia_preview/criteo_api_retailmedia_preview/__init__.py
@@ -8,7 +8,7 @@
 """
 
 
-__version__ = "0.0.250708"
+__version__ = "0.0.250709"
 
 # import ApiClient
 from criteo_api_retailmedia_preview.api_client import ApiClient
